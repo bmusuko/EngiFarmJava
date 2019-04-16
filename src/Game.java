31e2cfd2a9837e820611e912e73feffc7c30ded8
bmusuko <bram.musuko@gmail.com>
2019-04-16 19:44:19 +0700

    rapih

---
+++ src/Game.java
@@ -0,0 +1,28 @@
+class Game{
+	public Game(){
+
+	}	
+
+	
+	public void Game::listCommand(){
+		cout << "!--- LIST COMMAND ---!"<<endl;
+		cout<<"talk up"<<endl;
+		cout<<"talk left"<<endl;
+		cout<<"talk right"<<endl;
+		cout<<"talk down"<<endl;
+		cout<<"interact up"<<endl;
+		cout<<"interact left"<<endl;
+		cout<<"interact right"<<endl;
+		cout<<"interact down"<<endl;
+		cout<<"kill up"<<endl;
+		cout<<"kill left"<<endl;
+		cout<<"kill right"<<endl;
+		cout<<"kill down"<<endl;
+		cout<<"grow"<<endl;
+		cout<<"status"<<endl;
+		cout<<"tutorial"<<endl;
+		cout<<"keterangan"<<endl;
+		cout<<"listCommand"<<endl;
+		cout<<"exit"<<endl<<endl;
+	}
+}
