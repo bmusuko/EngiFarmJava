f143799f10b733f803c1a3f0f027f7fd88a38dfd
Valentino Simamora <valent.14021999@gmail.com>
2019-04-19 22:20:45 +0700

    Update Gameplay.java

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -1,35 +1,26 @@
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.MouseInfo;
-import java.awt.Point;
-import java.awt.PointerInfo;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
+import java.awt.*;
 import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import javax.swing.JPanel;
-import javax.swing.Timer;
 
-public class Gameplay extends JPanel implements ActionListener, KeyListener{
-    private static final long serialVersionUID = 1L;
-    private Game gim;
-    private Timer time;
+import javax.imageio.ImageIO;
+import java.io.*;
+import javax.swing.*;
+
+// ini ada perubahan inspirasi gatau bisa apa nggak, wkwkwk
+public class Gameplay extends JFrame implements KeyListener{
+    private static JLabel[][] peta = new JLabel[10][11];
+    private static JPanel listInventory;
+    private static JPanel maps;
+    private static JPanel top;
+    private static JPanel right;
 
     public Gameplay() {
-        HandleImage.initImage();
-        gim = new Game();
-        // addMouseListener(this);
-        addKeyListener(this);
-        setFocusable(true);
-        setFocusTraversalKeysEnabled(false);
-    }
+        setTitle("Engi's Farm by senjaGurau");
+        setSize(1280, 720);
 
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
 
     }
 }
+
+
+
+    
