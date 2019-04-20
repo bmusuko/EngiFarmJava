f58dcc2f87102d98dff76861c10271dec74b6c3f
Putu Gde Aditya Taguh Widiana <42746060+putugde@users.noreply.github.com>
2019-04-20 16:37:43 +0700

    Add files via upload

--- src/GameMain.java
+++ src/GameMain.java
@@ -1,4 +1,6 @@
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.WindowConstants;
 
 /**
@@ -17,8 +19,18 @@ public class GameMain extends JFrame {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
+        JLabel title = new JLabel(new ImageIcon("resource/truck.png"));
+        JLabel[][] map = new JLabel[10][11];
+        for (int i = 0;i<10;i++){
+            for (int j = 0;j<11;j++){
+                map[i][j] = new JLabel(i+" "+j);
+                map[i][j].setBounds(50+(j*60),100+(i*60),60,60);
+                frame.add(map[i][j]);
+            }
+        }
         // Masukin obejct game disini
         // Gameplay gim = new Gameplay();
+        frame.setLayout(null);
         frame.setBounds(10, 10, 1280, 800);
         frame.setTitle("Engi's Farm by: SenjaGurau");
         frame.setResizable(false);
@@ -26,9 +38,14 @@ public class GameMain extends JFrame {
         // frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
+        title.setBounds(0, 0, 1280, 100);
+        frame.add(title);
         // nanti object nya game, di add ke frame
         // frame.add(gim);
-    }
+        
 
-    
+
+
+    }
 }
